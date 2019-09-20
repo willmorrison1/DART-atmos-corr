b98fd81c391af76f43f99bcac4dcbcb848331469
willmorrison1 <51793934+willmorrison1@users.noreply.github.com>
2019-09-20 15:29:49 +0100

    add tested script for writing z buffer in Blender

---
+++ code/operational/MLOS/writeCameraPathLengths.py
@@ -0,0 +1,52 @@
+#get a depth map for all cameras in Blender workspace.
+#inspired by https://blender.stackexchange.com/questions/42579/render-depth-map-to-image-with-python-script 
+#by default we assume there is only one "scene" in the Blender environment
+
+import os
+import bpy
+#set the output directory
+outDir = "cameraPathLengths/"
+
+scene = bpy.context.scene
+
+#set renderer options
+bpy.data.scenes[0].render.image_settings.file_format = "TIFF"
+bpy.data.scenes[0].render.image_settings.color_depth = "16"
+bpy.data.scenes[0].render.image_settings.color_mode = "RGB"
+bpy.data.scenes[0].render.image_settings.tiff_codec = "NONE"
+
+#set min render distance to zero
+minRenderDist = 0
+#set maximum render distance to 5000 m
+maxRenderDist = 5000
+#Set up rendering of depth map:
+bpy.context.scene.use_nodes = True
+tree = bpy.context.scene.node_tree
+links = tree.links
+
+print("Saving to " + outDir + "...")
+
+#clear default nodes
+for n in tree.nodes:
+    tree.nodes.remove(n)
+	
+#configure the renderer nodes
+rl = tree.nodes.new('CompositorNodeRLayers')
+map = tree.nodes.new(type = "CompositorNodeMath")
+map.operation = "DIVIDE"
+map.inputs[1].default_value = maxRenderDist
+links.new(rl.outputs[2], map.inputs[0])
+compositeOutput = tree.nodes.new(type = "CompositorNodeComposite")
+links.new(map.outputs[0], compositeOutput.inputs[0])
+
+#iterate over each camera and render the z buffer
+for ob in scene.objects:
+    if ob.type == 'CAMERA':
+        bpy.context.scene.camera = ob
+        ob.data.clip_start = minRenderDist
+        ob.data.clip_end = maxRenderDist
+        filePath = outDir + "/" + ob.name + ".tiff"
+        print(filePath)
+        bpy.data.scenes['Scene'].render.filepath = filePath
+        bpy.ops.render.render(write_still = True) 
+
